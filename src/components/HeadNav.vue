406c4207f5c8b896aaaf9e449950237d66993c4f
Tourssliver <1341198325@qq.com>
2019-12-12 23:17:51 +0800

    first commit

---
+++ src/components/HeadNav.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeadNav",
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+      <el-container>
+          <el-header>
+            <head-nav></head-nav>
+          </el-header>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeadNav from "../components/HeadNav";
+
+export default {
+  name: 'home',
+  components: {
+    HeadNav
+  },
+
+}
+</script>
