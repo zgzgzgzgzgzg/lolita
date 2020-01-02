f8b93e42f21a882c24b03786a0577673852c81ca
maxiang <1341198325@qq.com>
2020-01-02 18:10:28 +0800

    头部user完成

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -5,6 +5,11 @@
             <ul class="nav-logo-ul">
                 <li class="nav-logo-ul-li">
                     <a href="" class="link">
+                        首页
+                    </a>
+                </li>
+                <li class="nav-logo-ul-li">
+                    <a href="" class="link">
                         lolita
                     </a>
                 </li>
@@ -18,13 +23,24 @@
                         JK
                     </a>
                 </li>
+                <li class="nav-logo-ul-li">
+                    <a href="" class="link">
+                        下载APP
+                    </a>
+                </li>
             </ul>
         </div>
-        <div class="nav-link">
-
-        </div>
         <div class="nav-user">
-
+            <div class="nav-user-search">
+                <i class="el-icon-search"></i>
+            </div>
+            <div class="nav-user-notification">
+                <i class="el-icon-bell"></i>
+            </div>
+            <div class="nav-user-avatar">
+<!--                <el-avatar icon="el-icon-user-solid"></el-avatar>-->
+                <i class="el-icon-user"></i>
+            </div>
         </div>
     </div>
 </template>
@@ -50,6 +66,8 @@
         position: absolute;
         width: 100%;
         height: 56px;
+        display: flex;
+        justify-content:space-between;
         .nav-logo{
             box-sizing: border-box;
             margin: 0;
@@ -63,13 +81,34 @@
                     margin-right: 12px;
                     .link{
                         font-size: 14px;
-                        color: #ffffff;
+                        color: #000000;
                         text-shadow: 0 1px 1px rgba(0,0,0,.3);
                         white-space: nowrap;
                         display: flex;
                         text-decoration: none;
                     }
                 }
+            }
+        }
+        .nav-user{
+            box-sizing: border-box;
+            display: flex;
+            flex-shrink: 0;
+            .nav-user-search{
+                display: flex;
+                align-items: center;
+                margin-left: 5px;
+            }
+            .nav-user-notification{
+                display: flex;
+                align-items: center;
+                margin-left: 5px;
+            }
+            .nav-user-avatar{
+                display: flex;
+                align-items: center;
+                margin-right: 24px;
+                margin-left: 5px;
             }
         }
     }
